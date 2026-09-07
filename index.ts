65e79cbe34f73a04b7569cb7523700cc91b137fc
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-07 16:39:09 -0300

    Add README and license

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
+/* opencode-zellij-renamer: OpenCode plugin that renames the Zellij session based on your first message
+ * Copyright (C) 2026 Eric Rodrigues Pires
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <https://www.gnu.org/licenses/>. */
+
 import { type Plugin, tool } from "@opencode-ai/plugin";
 
 const PROMPT = `You are a Zellij terminal session name generator. You output ONLY a name. Nothing else. You should always be invoked in response to the user's first message.
